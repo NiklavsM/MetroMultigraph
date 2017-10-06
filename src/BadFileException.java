1b867830a184a608c4679215e32a11ab84685602
Niklavs Meiers <niklavs@burti.lv>
2017-10-06 17:22:25 +0100

    Better error

--- src/BadFileException.java
+++ src/BadFileException.java
@@ -1,6 +1,6 @@
 
 public class BadFileException extends Exception {
 	public BadFileException(String exception){
-		System.out.println("Problem occurred while reading in file");
+		System.out.println(exception);
 	}
 }
